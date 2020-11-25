a1fea1dd43ac84107fee6a94180ca5fbd2946b99
Kashif Soofi <kashif.soofi+git@gmail.com>
2020-11-25 15:29:42 +0000

    Fixed Startup and SplashWindow xaml (WIP) renamed bygfoot c project
    Signed-off-by: Kashif Soofi <kashif.soofi+git@gmail.com>

--- Bygfoot/StartupWindow.xaml.cs
+++ Bygfoot/StartupWindow.xaml.cs
@@ -24,45 +24,10 @@
 
     public class StartupWindowViewModel : INotifyPropertyChanged
     {
-        private readonly IHintProvider hintProvider;
-        private string currentHint;
-        private int currentHintNumber;
-
         public StartupWindowViewModel()
         {
-            hintProvider = new HintProvider();
-            currentHintNumber = hintProvider.GetLastHintNumber();
-            currentHint = hintProvider.GetHint(currentHintNumber++);
-        }
-
-        public string HintText
-        {
-            get => currentHint;
-            set
-            {
-                currentHint = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(HintText)));
-            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
-
-        public void NextButtonClicked() => HintText = hintProvider.GetHint(currentHintNumber++);
-
-        public void PrevButtonClicked() => HintText = hintProvider.GetHint(currentHintNumber--);
-    }
-
-    public interface IHintProvider
-    {
-        int GetTotalHints();
-        int GetLastHintNumber();
-        string GetHint(int number);
-    }
-
-    public class HintProvider : IHintProvider
-    {
-        public int GetTotalHints() => 10;
-        public int GetLastHintNumber() => 1;
-        public string GetHint(int hintNumber) => $"This is hint {hintNumber}";
     }
 }
